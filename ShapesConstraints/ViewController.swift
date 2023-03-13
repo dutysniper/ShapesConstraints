ecbb7df923cbd53379f3331ba0916b6683b6dfce
dutysniper <natarov.constantin@yandex.ru>
2023-03-13 22:42:58 +0300

    Initial commit

---
+++ ShapesConstraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShapesConstraints
+//
+//  Created by Константин Натаров on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
